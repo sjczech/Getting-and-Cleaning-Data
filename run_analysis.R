b9826c1b34488f5c24dde8b433014e5acf2fefa6
sjczech <sjczech@gmail.com>
2015-07-26 14:35:56 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -4,8 +4,8 @@ file <- "uci_har.zip"
 path <- "UCI HAR Dataset"
 
 #functions
-#read table from file and apply columns
-retrieveTable <- function(filename, cols = NULL) {
+#read data from file and apply columns
+f1 <- function(filename, cols = NULL) {
   data <- data.frame()
   a <- unz(file, paste(path, filename, sep="/"))
   if(is.null(cols)){
@@ -18,10 +18,10 @@ retrieveTable <- function(filename, cols = NULL) {
 }
 
 #read dataset from file
-retrieveDataset <- function(type, features){
-    subject <- retrieveTable(paste(type,"/","subject_",type,".txt",sep=""),"id")
-    y <- retrieveTable(paste(type,"/","y_",type,".txt",sep=""),"activity")    
-    x <- retrieveTable(paste(type,"/","X_",type,".txt",sep=""),features$V2) 
+f2 <- function(type, features){
+    subject <- f1(paste(type,"/","subject_",type,".txt",sep=""),"id")
+    y <- f1(paste(type,"/","y_",type,".txt",sep=""),"activity")    
+    x <- f1(paste(type,"/","X_",type,".txt",sep=""),features$V2) 
     
     return (cbind(subject, y, x)) 
   } 
@@ -31,27 +31,22 @@ download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUC
 unzip(file)
 
 #create column names for train and test datasets from features.txt
-features <- retrieveTable("features.txt")
+features <- f1("features.txt")
 
-#load test and train datasets
-train <- retrieveDataset("train",features)
-test <- retrieveDataset("test",features)
-
-#merge train and test datasets
+#load and merge test and train datasets
+train <- f2("train",features)
+test <- f2("test",features)
 merged_data <- rbind(train, test)
 
 #rearrange the data using id
 merged_data <- arrange(merged_data, id)
 
 #assign labels for activities
-activity_labels <- retrieveTable("activity_labels.txt")
+activity_labels <- f1("activity_labels.txt")
 merged_data$activity <- factor(merged_data$activity, levels=activity_labels$V1, labels=activity_labels$V2)
 
-#extract only the measurements on the mean and standard deviation 
+#subset the measurements on the mean and standard deviation 
 dataset_std_mean <- merged_data[,c(1,2,grep("std", colnames(merged_data)), grep("mean", colnames(merged_data)))]
-
-#save first dataset
-write.table(dataset_std_mean, "dataset_std_mean.txt", row.names = FALSE)
 
 #create a second dataset with the average of each variable for each activity and subject 
 dataset_avg <- ddply(dataset_std_mean, .(id, activity), .fun=function(x){ colMeans(x[,-c(1:2)]) })
@@ -59,5 +54,6 @@ dataset_avg <- ddply(dataset_std_mean, .(id, activity), .fun=function(x){ colMea
 #append "_avg" to column names
 colnames(dataset_avg)[-c(1:2)] <- paste(colnames(dataset_avg)[-c(1:2)], "_avg", sep="")
 
-#save second dataset
+#save both datasets to text
+write.table(dataset_std_mean, "dataset_std_mean.txt", row.names = FALSE)
 write.table(dataset_avg, "dataset_avg.txt", row.names = FALSE)
